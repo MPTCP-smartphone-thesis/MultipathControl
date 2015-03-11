823c5741b2a5e4b050d63323615f783c43433f0d
Matthieu Baerts <matttbe@gmail.com>
2015-03-11 11:39:12 +0100

    boot: Launch the main activity

---
+++ MultipathControl/src/be/uclouvain/multipathcontrol/Boot.java
@@ -0,0 +1,16 @@
+package be.uclouvain.multipathcontrol;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Boot extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+			Intent startServiceIntent = new Intent(context, MainActivity.class);
+			startServiceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			context.startActivity(startServiceIntent);
+		}
+	}
+}
