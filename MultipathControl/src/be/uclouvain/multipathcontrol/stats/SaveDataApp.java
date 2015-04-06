eb7f1ff50c4912f9485878cda54429a3b1998e23
Matthieu Baerts <matttbe@gmail.com>
2015-04-06 17:06:06 +0200

    SaveDataApp: save info of the app

---
+++ MultipathControl/src/be/uclouvain/multipathcontrol/stats/SaveDataApp.java
@@ -0,0 +1,51 @@
+package be.uclouvain.multipathcontrol.stats;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import be.uclouvain.multipathcontrol.global.Config;
+
+public class SaveDataApp extends SaveDataAbstract {
+
+	public static final String PREFS_VERSION_NAME = "versionName";
+	public static final String PREFS_VERSION_CODE = "versionCode";
+	public static final String PREFS_LAST_UPDATE  = "lastUpdate";
+	public static final String PREFS_STATUS       = "enable";
+	public static final String PREFS_DEFAULT_DATA = "defRouteCell";
+	public static final String PREFS_DATA_BACKUP  = "cellBackup";
+	public static final String PREFS_SAVE_BATTERY = "saveBattery";
+	public static final String PREFS_TCPCC        = "TCPCCAlgo";
+
+	public SaveDataApp(Context context) {
+		super(context, StatsCategories.APP);
+
+		setVersion(context);
+		setPref();
+
+		save();
+	}
+
+	private void setVersion(Context context) {
+		PackageInfo info;
+		try {
+			info = context.getPackageManager().getPackageInfo(
+					context.getPackageName(), 0);
+		} catch (NameNotFoundException e) {
+			// should not happen... our package
+			e.printStackTrace();
+			return;
+		}
+		editor.putString(PREFS_VERSION_NAME, info.versionName);
+		editor.putInt(PREFS_VERSION_CODE, info.versionCode);
+		editor.putLong(PREFS_LAST_UPDATE, info.lastUpdateTime);
+	}
+
+	private void setPref() {
+		editor.putBoolean(PREFS_STATUS, Config.mEnabled);
+		editor.putBoolean(PREFS_DEFAULT_DATA, Config.defaultRouteData);
+		editor.putBoolean(PREFS_DATA_BACKUP, Config.dataBackup);
+		editor.putBoolean(PREFS_SAVE_BATTERY, Config.saveBattery);
+		editor.putString(PREFS_TCPCC, Config.tcpcc);
+	}
+
+}
